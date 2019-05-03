f249821ef4a901e64eb3483002e75121e6927fbb
unknown <Administrator@HUSHAN-4.fareast.corp.microsoft.com>
2019-05-03 12:10:27 +0800

    solve the issue of redirect fetch

--- client/src/pages/Consent.tsx
+++ client/src/pages/Consent.tsx
@@ -7,28 +7,60 @@ interface IProps {
 
 interface IStates {}
 export default class Consent extends React.Component<IProps, IStates> {
+    params: URLSearchParams;
+    transactionId: string | null;
+    constructor(props: IProps) {
+        super(props);
+        this.params = new URLSearchParams(this.props.location.search);
+        this.transactionId = this.params.get("transactionID");
+    }
     render(): React.ReactElement<any> {
-        const params: URLSearchParams = new URLSearchParams(this.props.location.search);
-        const transactionId: string | null = params.get("transactionID");
-        if (!transactionId) {
+        if (!this.transactionId) {
             return <NotFound location={this.props.location}/>;
         } else {
             return (
                 <div className="container">
                     <div className="page-header">
-                        <p><h3>Hi {params.get("email")},</h3></p>
-                        <p><h3>{params.get("client_name")} is requesting access to your account.</h3></p>
-                        <p><h3>Do you approve?</h3></p>
+                        <h3>Hi {this.params.get("email")},</h3>
+                        <h3>{this.params.get("client_name")} is requesting access to your account.</h3>
+                        <h3>Do you approve?</h3>
                     </div>
-                    <form className="form-horizontal" action="/oauth2/authorize/decision" method="post">
-                        <input name="transaction_id" type="hidden" value={transactionId} />
+                    <div className="form-horizontal">
+                        <input name="transaction_id" type="hidden" value={this.transactionId} />
                         <div className="btn-toolbar">
-                            <button className="btn btn-success" type="submit" value="Allow" id="allow">Allow</button>
-                            <button className="btn btn-warning" type="submit" value="Deny" id="deny">Deny</button>
+                            <button className="btn btn-success" onClick={this._allow}>Allow</button>
+                            <button className="btn btn-warning">Deny</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             );
         }
+    }
+
+    private _allow = () => {
+        console.log("this._allow is called");
+        const url: string = "http://localhost:3000/oauth2/authorize/decision";
+        const body: any = {transaction_id: this.transactionId};
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow", // manual, *follow, error
+            body: JSON.stringify(body), // body data type must match "Content-Type" header
+        }).then((response: Response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject({
+                    status: response.status,
+                    statusText: response.statusText
+                });
+            }
+        }).then((json: any) => {
+            console.log("response.json is " + JSON.stringify(json));
+        }, (error: any) => {
+            console.log(`error is ${error.status}: ${error.statusText}`);
+        });
     }
 }
--- client/src/utils/apiCaller.ts
+++ client/src/utils/apiCaller.ts
@@ -1,10 +1,10 @@
 import fetch from "isomorphic-fetch";
 import RequestResult from "../models/RequestResult";
 
-export const API_URL = `http://localhost:${process.env.PORT}/api`; // TODO: switch between debug and product
+export const API_URL = `http://localhost:${process.env.PORT}`; // TODO: switch between debug and product
 
 function callApi(endpoint: string, method = "get", body: any): Promise<RequestResult> {
-  return fetch(`${API_URL}/${endpoint}`, {
+  return fetch(`${API_URL}${endpoint}`, {
     headers: { "content-type": "application/json" },
     method,
     body: JSON.stringify(body),
