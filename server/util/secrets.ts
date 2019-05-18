64a74fac417fc4a51c0519d2c0a70f2860a82fd7
unknown <Administrator@HUSHAN-4.fareast.corp.microsoft.com>
2019-05-18 22:30:57 +0800

    revise the readme.md

--- server/util/secrets.ts
+++ server/util/secrets.ts
@@ -1,14 +1,13 @@
-import logger from "./logger";
 import dotenv from "dotenv";
 
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
+const prod = ENVIRONMENT === "production";
 
 if (prod) {
-    logger.debug("Using .env.production file to supply config environment variables");
+    console.debug("Using .env.production file to supply config environment variables");
     dotenv.config({ path: ".env.production" });
 } else {
-    logger.debug("Using .env.development file to supply config environment variables");
+    console.debug("Using .env.development file to supply config environment variables");
     dotenv.config({ path: ".env.development" });  // you can delete this after you create your own .env file!
 }
 
@@ -16,11 +15,11 @@ export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const MONGODB_URI = /*prod ? process.env["MONGODB_URI"] : */process.env["MONGODB_URI_LOCAL"];
 
 if (!SESSION_SECRET) {
-    logger.error("No client secret. Set SESSION_SECRET environment variable.");
+    console.error("No client secret. Set SESSION_SECRET environment variable.");
     process.exit(1);
 }
 
 if (!MONGODB_URI) {
-    logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
+    console.error("No mongo connection string. Set MONGODB_URI environment variable.");
     process.exit(1);
 }
